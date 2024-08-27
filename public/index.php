b940c2625b14a3b82388ebee8a5230812e7bc31b
Ryan Levsey <rlevsey@santaclarautah.gov>
2024-08-27 16:30:57 -0400

    404 route and dark theme added

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+error_reporting(0);
 require("../vendor/autoload.php");
 $router = require("../src/Routes/index.php");
